b45172a65f129f39155edb5e418430a78aa3d59b
xXmarcosX <marcos.v3567@gmail.com>
2024-06-13 23:22:25 -0300

    adicionado método para deletar carro

--- src/components/SelectedCar/SelectedCar.jsx
+++ src/components/SelectedCar/SelectedCar.jsx
@@ -1,11 +1,29 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const SelectedCar = ({car}) => {
+
+  const navigate = useNavigate()
+
+  const handleDelete = (id) => {
+    fetch(`http://localhost:4000/car/${id}`,
+      {
+        method: 'DELETE'
+      }
+    ).then((response) => {
+      console.log(response.status)
+      alert('ain tu vai deletar o carro pain')
+      navigate('/carros')
+    })
+  }
+
   return (
     <>
       <ul>
         <li>{car.mark}</li>
         <li>{car.model}</li>
+
+        <button onClick={() => handleDelete(car._id)}>Deletar</button>
       </ul>
     </>
   )
