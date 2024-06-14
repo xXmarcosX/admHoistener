7364f7dbb3aba88b1daf3ba382e8521f517c05ab
xXmarcosX <marcos.v3567@gmail.com>
2024-06-13 21:20:13 -0300

    adicionada rota para os detalhes do carro

---
+++ src/routes/Car/Car.jsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import SelectedCar from '../../components/SelectedCar/SelectedCar';
+
+const Car = () => {
+
+    const [cars, setCars] = useState([]);
+    const id = useParams()
+
+    useEffect(() => {
+        fetch('http://localhost:4000/cars')
+            .then(response => response.json())
+            .then(data => setCars(data))
+            .catch(error => console.error('Erro ao obter carros:', error));
+    }, []);
+
+    const selectedCar = cars.find((car) => {
+        return car._id === id.id
+    })
+
+  return (
+    <>
+      {selectedCar ? (
+         <SelectedCar car={selectedCar}/>
+      ):
+      <h1>car not found</h1>}
+    </>
+  )
+}
+
+export default Car
